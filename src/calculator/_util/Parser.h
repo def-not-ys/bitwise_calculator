5adfad56573daa1ca9fd5fc153c1ed70d8bb5511
def-not-ys <luys1201@gmail.com>
2020-12-31 14:20:44 -0800

    implemented prioritization in parser

--- src/calculator/_util/Parser.h
+++ src/calculator/_util/Parser.h
@@ -13,12 +13,20 @@ namespace AST {
             void visit(Multiply* node);
             void visit(Divide* node);
 
+            void prioritize(std::string& str);
+
             int test(); // !!!
         protected:
             int _findExpressionsLocation(std::string& str); 
-            int _findRightBracket(std::string &str);
-            int _findFirstTerm(std::string &str);
+            int _findRightBracket(const std::string &str, int sub_start, int sub_end);
+            int _findLeftBracket(const std::string &str, int sub_start, int sub_end);
+            int _findTermStart(std::string &str, int loc);
+            int _findTermEnd(std::string &str, int loc);
+            void _checkImplicitMultiply(std::string& str);
+            void _prioritize_left(std::string& str, int i);
+            void _prioritize_right(std::string& str, int i);
             bool _isOperator(const char ch);
+            bool _isNumber(const char ch);
 
     };
 }
